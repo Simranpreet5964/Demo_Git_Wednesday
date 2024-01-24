6ce50049a9521df0e510ba4c9a5566945dd376e2
Simranpreet5964 <simranpreet5964@gmail.com>
2024-01-24 09:13:06 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/demo_git_wednesday/Demo_Git_Wednesday.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demo_git_wednesday;
+
+/**
+ *
+ * @author Arwinder
+ */
+public class Demo_Git_Wednesday {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
